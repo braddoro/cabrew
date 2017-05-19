ef9c7249b4e72fc050aa2138ff25ddc48f21cf7c
brad <braddoro@gmail.com>
2017-05-19 16:57:20 -0400

    altered title method

--- smart/library.js
+++ smart/library.js
@@ -11,77 +11,73 @@ function copyValues(stringIn){
 	return stringOut;
 }
 function errorTitle(code) {
-	var title = "";
-	switch(code) {
-	case isc.RPCResponse.STATUS_SUCCESS: // 0
-		title = "Success";
-		break;
-	case isc.RPCResponse.STATUS_OFFLINE: // 1
-		title = "Offline";
-		break;
-	case isc.RPCResponse.STATUS_FAILURE: // -1
-		title = "Failure";
-		break;
-	case isc.RPCResponse.STATUS_AUTHORIZATION_FAILURE: // -3
-		title = "Authorization Failure";
-		break;
-	case isc.RPCResponse.STATUS_VALIDATION_ERROR: // -4
-		title = "Status Validation Error";
-		break;
-	case isc.RPCResponse.STATUS_LOGIN_INCORRECT: // -5
-		title = "Status Login Incorrect";
-		break;
-	case isc.RPCResponse.STATUS_MAX_LOGIN_ATTEMPTS_EXCEEDED: // -6
-		title = "Status Max Login Attempts Exceeded";
-		break;
-	case isc.RPCResponse.STATUS_LOGIN_REQUIRED: // -7
-		title = "Status Login Required";
-		break;
-	case isc.RPCResponse.STATUS_LOGIN_SUCCESS: // -8
-		title = "Status Login Success";
-		break;
-	case isc.RPCResponse.STATUS_UPDATE_WITHOUT_PK_ERROR: // -9
-		title = "Status Update Without PK Error";
-		break;
-	case isc.RPCResponse.STATUS_TRANSACTION_FAILED: // -10
-		title = "Status Transaction Failed";
-		break;
-	case isc.RPCResponse.STATUS_MAX_FILE_SIZE_EXCEEDED: // -11
-		title = "Status Max File Size Exceeded";
-		break;
-	case isc.RPCResponse.STATUS_MAX_POST_SIZE_EXCEEDED: // -12
-		title = "Status Max Post Size Exceeded";
-		break;
-	case isc.RPCResponse.STATUS_FILE_REQUIRED_ERROR: // -15
-		title = "Status File Required Error";
-		break;
-	case isc.RPCResponse.INVALID_RESPONSE_FORMAT: // -16
-		title = "Invalid Response Format";
-		break;
-	case isc.RPCResponse.STATUS_TRANSPORT_ERROR: // -90
-		title = "Status Transport Error";
-		break;
-	case isc.RPCResponse.STATUS_UNKNOWN_HOST_ERROR: // -91
-		title = "Status Unknown Host Error";
-		break;
-	case isc.RPCResponse.STATUS_CONNECTION_RESET_ERROR: // -92
-		title = "Status Connection Reset Error";
-		break;
-	case isc.RPCResponse.STATUS_SERVER_TIMEOUT: // -100
-		title = "Status Server Timeout";
-		break;
-	case isc.RPCResponse.STATUS_ERROR_DATA_ACCESS: // -110
-		title = "General Data Access Error";
-		break;
-	case isc.RPCResponse.STATUS_SERVER_CONNECTION_ERROR: // -111
-		title = "Server Connection Error";
-		break;
-	case isc.RPCResponse.STATUS_SETUP_DATA_ERROR: // -112
-		title = "Server Setup Error";
-		break;
-	default:
-		title = "Unknown Error Type";
-		break;
+	if(code === isc.RPCResponse.STATUS_SUCCESS){ // 0
+		return "Success";
 	}
+	if(code === isc.RPCResponse.STATUS_OFFLINE){ // 1
+		return "Offline";
+	}
+	if(code === isc.RPCResponse.STATUS_FAILURE){ // -1
+		return "Failure";
+	}
+
+	if(code === isc.RPCResponse.STATUS_AUTHORIZATION_FAILURE){ // -3
+		return "Authorization Failure";
+	}
+	if(code === isc.RPCResponse.STATUS_VALIDATION_ERROR){ // -4
+		return "Validation Error";
+	}
+	if(code === isc.RPCResponse.STATUS_LOGIN_INCORRECT){ // -5
+		return "Login Incorrect";
+	}
+	if(code === isc.RPCResponse.STATUS_MAX_LOGIN_ATTEMPTS_EXCEEDED){ // -6
+		return "Max Login Attempts Exceeded";
+	}
+	if(code === isc.RPCResponse.STATUS_LOGIN_REQUIRED){ // -7
+		return "Login Required";
+	}
+	if(code === isc.RPCResponse.STATUS_LOGIN_SUCCESS){ // -8
+		return "Login Success";
+	}
+	if(code === isc.RPCResponse.STATUS_LOGIN_SUCCESS){ // -9
+		return "Update Without PK Error";
+	}
+	if(code === isc.RPCResponse.STATUS_TRANSACTION_FAILED){ // -10
+		return "Transaction Failed";
+	}
+	if(code === isc.RPCResponse.STATUS_MAX_FILE_SIZE_EXCEEDED){ // -11
+		return "Max File Size Exceeded";
+	}
+	if(code === isc.RPCResponse.STATUS_MAX_POST_SIZE_EXCEEDED){ // -12
+		return "Max Post Size Exceeded";
+	}
+	if(code === isc.RPCResponse.STATUS_FILE_REQUIRED_ERROR){ // -15
+		return "File Required Error";
+	}
+	if(code === isc.RPCResponse.INVALID_RESPONSE_FORMAT){ // -16
+		return "Invalid Response Format";
+	}
+	if(code === isc.RPCResponse.STATUS_TRANSPORT_ERROR){ // -90
+		return "Transport Error";
+	}
+	if(code === isc.RPCResponse.STATUS_UNKNOWN_HOST_ERROR){ // -91
+		return "Unknown Host Error";
+	}
+	if(code === isc.RPCResponse.STATUS_CONNECTION_RESET_ERROR){ // -92
+		return "Connection Reset Error";
+	}
+	if(code === isc.RPCResponse.STATUS_SERVER_TIMEOUT){ // -100
+		return "Server Timeout";
+	}
+	if(code === isc.RPCResponse.STATUS_ERROR_DATA_ACCESS){ // -110
+		return "General Data Access Error";
+	}
+	if(code === isc.RPCResponse.STATUS_SERVER_CONNECTION_ERROR){ // -111
+		return "Server Connection Error";
+	}
+	if(code === isc.RPCResponse.STATUS_SETUP_DATA_ERROR){ // -112
+		return "Server Setup Error";
+	}
+	title = "Unknown Error Type";
     return title;
 }
