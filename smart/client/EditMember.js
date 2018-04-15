bdf26d7bc3147964a93e567d50d7e72f64e05c34
brad <braddoro@gmail.com>
2018-04-15 19:43:04 -0400

    Altered the call to the data source.

--- smart/client/EditMember.js
+++ smart/client/EditMember.js
@@ -4,14 +4,17 @@ isc.defineClass("EditMember", "myWindow").addProperties({
 		this.Super("initWidget", arguments);
 		this.EditMemberDF = isc.myDynamicForm.create({
 			parent: this,
-			dataSource: isc.Members.membersDS
+			dataSource: isc.Members.membersDS,
+			updateData: function(updatedRecord, callback, requestProperties){
+				return this.Super("updateData", [updatedRecord, callback, requestProperties]);
+			}
 		});
 		this.EditMemberBT = isc.myIButton.create({
 			parent: this,
 			title: "Save",
 			align: "center",
 			click: function(){
-				this.parent.EditMemberDS.updateData(this.parent.EditMemberDF.getValues());
+				this.parent.EditMemberDF.updateData(this.parent.EditMemberDF.getValues());
 			}
 		});
 		this.addItem(isc.myVLayout.create({members: [this.EditMemberDF, this.EditMemberBT]}));
