5404ec1e1f3ff5eef11591aa8016c2bdb69f1d79
brad <braddoro@gmail.com>
2019-09-28 17:53:44 -0400

    Delete old code.

--- bluehost/smart/server/ItemGroups.php
+++ bluehost/smart/server/ItemGroups.php
@@ -12,31 +12,9 @@ if(!$db->isConnected()){
 }
 $pkval = (isset($_REQUEST[$primaryKey])) ? intval($_REQUEST[$primaryKey]) : NULL;
 $operationType = (isset($_REQUEST['operationType'])) ? $_REQUEST['operationType'] : 'fetch';
-// $access_array = parse_ini_file('access.ini', true);
-// $accesslist = $access_array['access'][basename(__FILE__)];
-// if((!substr_count($accesslist,$operationType))){
-// 	$response = array('status' => -4, 'errorMessage' => $conn->getMessage(2, $operationType));
-// 	echo json_encode($response);
-// 	exit(1);
-// }
-// if(($operationType == 'update' || $operationType == 'remove') && is_null($pkval)){
-// 	$response = array('status' => -1, 'errorMessage' => $conn->getMessage(1, $operationType));
-// 	echo json_encode($response);
-// 	exit(1);
-// }
 switch($operationType){
 case 'fetch':
 	$where = '1=1';
-	// if(isset($_REQUEST['status'])){
-	// 	$qStr = $db->qStr($_REQUEST['status'], true);
-	// 	$where .= " and status = $qStr ";
-	// }
-	// if(isset($_REQUEST['eventTypeID'])){
-	// 	$where .= ' and eventTypeID = ' . intval($_REQUEST['eventTypeID']);
-	// }
-	// if(isset($_REQUEST['step'])){
-	// 	$where .= " and step like '%" . $_REQUEST['step'] . "%' ";
-	// }
 	break;
 case 'add':
 	$data = array('table' => $table, 'primaryKey' => $primaryKey, 'newvals' => $_REQUEST);
@@ -48,7 +26,6 @@ case 'add':
 case 'update':
 	$data = array('table' => $table, 'primaryKey' => $primaryKey, 'newvals' => $_REQUEST);
 	$record = $conn->buildRecordset($data);
-	// echo json_encode($record);
 	$where = $primaryKey . '=' . $pkval;
 	$db->AutoExecute($table, $record, DB_AUTOQUERY_UPDATE, $where);
  	break;
--- bluehost/smart/server/SaveEntry.php
+++ bluehost/smart/server/SaveEntry.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'Connect.php';
-// require_once 'SiteLog.php';
 $table = 'members';
 $primaryKey = 'memberID';
 $conn = new Connect();
@@ -12,18 +11,6 @@ if(!$db->isConnected()){
 }
 $pkval = (isset($_REQUEST[$primaryKey])) ? intval($_REQUEST[$primaryKey]) : NULL;
 $operationType = (isset($_REQUEST['operationType'])) ? $_REQUEST['operationType'] : 'fetch';
-$access_array = parse_ini_file('access.ini', true);
-$accesslist = $access_array['access'][basename(__FILE__)];
-if((!substr_count($accesslist,$operationType))){
-	$response = array('status' => -4, 'errorMessage' => $conn->getMessage(2, $operationType));
-	echo json_encode($response);
-	exit(1);
-}
-if(($operationType == 'update' || $operationType == 'remove') && is_null($pkval)){
-	$response = array('status' => -1, 'errorMessage' => $conn->getMessage(1, $operationType));
-	echo json_encode($response);
-	exit(1);
-}
 switch($operationType){
 case 'fetch':
 	$where = '1=1';
@@ -53,16 +40,6 @@ case 'remove':
 default:
 	break;
 }
-// $arr = array(
-// 	"pageName" => basename(__FILE__),
-// 	"action" => $operationType,
-// 	"tableName" => $table,
-// 	"primaryKeyID" => isset($pkval) ? intval($pkval) : null,
-// 	"primaryKey" => $primaryKey,
-// 	"fieldsVals" => var_export($_REQUEST, true),
-//	"userID" => (isset($_REQUEST['userID'])) ? intval($_REQUEST['userID']): 0
-// );
-// $r = siteLog($conn, $db, $arr);
 $sql = "select * from {$table} where {$where};";
 $response = $db->getAll($sql);
 if(!$response){
