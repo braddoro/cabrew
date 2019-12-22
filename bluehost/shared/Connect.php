d8d75ced8d6b898b079a610b29f778a38b70c330
brad <braddoro@gmail.com>
2019-12-22 18:58:18 -0500

    Remove dead code.

--- bluehost/shared/Connect.php
+++ bluehost/shared/Connect.php
@@ -3,7 +3,7 @@ class Connect {
 	protected $dbo;
 	public function conn() {
 		$hostname = gethostname();
-			require_once '../adodb5/adodb.inc.php'; // cabrew.org
+		require_once '../adodb5/adodb.inc.php'; // cabrew.org
 		$ini_array = parse_ini_file('server.ini', true);
 		$hostname = $ini_array['database']['hostname'];
 		$username = $ini_array['database']['username'];
