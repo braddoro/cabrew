fccdd6bcfa73f1b52081ba41c915b200b205d58c
brad <braddoro@gmail.com>
2019-12-22 19:20:44 -0500

    Modify how if statements are concatenated.

--- bluehost/shared/ClassDefaults.js
+++ bluehost/shared/ClassDefaults.js
@@ -62,18 +62,18 @@ isc.defineClass("myDataSource", "DataSource").addProperties({
 		var newResponse;
 		var message = "";
 
-		if(dsResponse.httpResponseCode &&
-			dsResponse.httpResponseCode < 200 &&
-			dsResponse.httpResponseCode > 299){
+		if(dsResponse.httpResponseCode
+			&& dsResponse.httpResponseCode < 200
+			&& dsResponse.httpResponseCode > 299){
 			status = dsResponse.httpResponseCode;
 		}
-		if(dsResponse.status &&
-			dsResponse.status !== isc.RPCResponse.STATUS_SUCCESS){
+		if(dsResponse.status
+			&& dsResponse.status !== isc.RPCResponse.STATUS_SUCCESS){
 			status = dsResponse.status;
 		}
-		if(status === isc.RPCResponse.STATUS_SUCCESS &&
-			data.status &&
-			data.status !== isc.RPCResponse.STATUS_SUCCESS){
+		if(status === isc.RPCResponse.STATUS_SUCCESS
+			&& data.status
+			&& data.status !== isc.RPCResponse.STATUS_SUCCESS){
 			status = data.status;
 		}
 
@@ -152,7 +152,7 @@ isc.defineClass("myListGrid", "ListGrid").addProperties({
 		if(!state){
 			stateStr = "(read only)";
 		}
-		if(this.name) {
+		if(this.name){
 			nameStr = this.name + " | ";
 		}
 		var title = nameStr + "" + rowsStr + " " + stateStr;
